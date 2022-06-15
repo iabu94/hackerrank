9ffb6815df16ef1334fcdd9df8ce2446403d7c89
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2022-06-16 03:00:38 +0530

    Compare the Triplets Challenge

--- 1 - Problem Solving/1.1 - Solve Me First/SolveMeFirst.cs
+++ 1 - Problem Solving/1.1 - Solve Me First/SolveMeFirst.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-class Solution {
+class Solution
+{
 
-    static int solveMeFirst(int a, int b) { 
-      // Hint: Type return a+b; below  
-      return a + b;
+    static int solveMeFirst(int a, int b)
+    {
+        // Hint: Type return a+b; below  
+        return a + b;
     }
 
-    static void Main(String[] args) {
+    static void Main(String[] args)
+    {
         int val1 = Convert.ToInt32(Console.ReadLine());
         int val2 = Convert.ToInt32(Console.ReadLine());
-        int sum = solveMeFirst(val1,val2);
+        int sum = solveMeFirst(val1, val2);
         Console.WriteLine(sum);
     }
-}      
+}
